fcc8fe2390e341b90b6e86c7e1b0fa45c5c85340
eshita1254 <86717164+eshita1254@users.noreply.github.com>
2021-08-02 20:33:58 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+type="text/javascript">
+function submitform(){
+    alert("Sending Json");
+    var xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action, true);
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    var j = {
+        "first_name":"binchen",
+        "last_name":"heris",
+    };
+    xhr.send(JSON.stringify(j));
